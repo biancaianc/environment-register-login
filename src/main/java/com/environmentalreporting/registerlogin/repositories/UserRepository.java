7c21501fa3ed678266192efe730bbc957fb3cfd8
biancaianc <biancaianc21@gmai.com>
2022-02-18 17:06:04 +0200

    initial commit

---
+++ src/main/java/com/environmentalreporting/registerlogin/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.environmentalreporting.registerlogin.repositories;
+
+import com.environmentalreporting.registerlogin.models.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
---
+++ src/test/java/com/environmentalreporting/registerlogin/RegisterLoginApplicationTests.java
@@ -0,0 +1,12 @@
+package com.environmentalreporting.registerlogin;
+
+import org.junit.Test;
+
+
+class RegisterLoginApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
